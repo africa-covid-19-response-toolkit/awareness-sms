25187ad812890190998c9f9f2fe6015d72432e81
Robera Worku <robgogoworku@gmail.com>
2020-03-18 16:23:18 +0300

    Added from field, updated readme

--- server/routes/index.js
+++ server/routes/index.js
@@ -7,7 +7,8 @@ let sendSms = require('../utils/sms-sender');
 router.post('/sendsms', async (req, res, next) => {
     const options = {
         to: req.body.to,
-        message: req.body.message
+        message: req.body.message,
+        from: req.body.from
     }
     let r = await sendSms(options);
     console.log(r);
