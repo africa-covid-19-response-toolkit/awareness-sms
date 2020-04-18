6050c6450f68aa84e3517cfc080032fb4a9af566
Robera Worku <robgogoworku@gmail.com>
2020-04-18 11:14:46 +0300

    API KEY visibility changed

--- server/config/constants.js
+++ server/config/constants.js
@@ -1,9 +1,12 @@
 // const apiKey = "fb865659e7d2317ce77826054e66f4c0f58b892a4de659c0ec3d067f75b974fc";
 // const username = "robgogo";
+const dotenv = require('dotenv');
+
+dotenv.config({path: 'server/.env'});
 
 const Keys = {
-    apiKey:"fb865659e7d2317ce77826054e66f4c0f58b892a4de659c0ec3d067f75b974fc",
-    username: "robgogo"   
+    apiKey: process.env.APIKEY,
+    username: process.env.USER
 };
 
 module.exports = Keys;
